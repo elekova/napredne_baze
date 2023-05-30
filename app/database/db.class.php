09b7611a56cf627311e31ff568749c08b86c127d
ikrizanec <ivanakrizanec@hotmail.com>
2023-05-30 11:54:02 +0200

    database

---
+++ app/database/db.class.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once __DIR__ . '/db_settings.php';
+
+class DB
+{
+	private static $db = null;
+
+	private function __construct() { }
+	private function __clone() { }
+
+	public static function getConnection() 
+	{
+		if( DB::$db === null )
+	    {
+            global $db_base, $db_user, $db_pass;
+	    	try
+	    	{
+		    	DB::$db = new PDO( $db_base, $db_user, $db_pass );
+		    	DB::$db-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		    }
+		    catch( PDOException $e ) { exit( 'PDO Error: ' . $e->getMessage() ); }
+	    }
+		return DB::$db;
+	}
+}
+
+?>
---
+++ app/database/db.settings.php
@@ -0,0 +1,8 @@
+<?php
+
+$db_base = 'mysql:host=rp2.studenti.math.hr;dbname=krizanec;s+charset=utf8';
+
+$db_user = 'student';
+$db_pass = 'pass.mysql';
+
+?>
