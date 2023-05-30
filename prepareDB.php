9b6b5c7973524ae45dc9544559189cefdae9ecf1
ikrizanec <ivanakrizanec@hotmail.com>
2023-05-30 19:13:02 +0200

    prepare

--- prepareDB.php
+++ prepareDB.php
@@ -127,8 +127,7 @@ try
 		'name varchar(50) NOT NULL,' .
         'city varchar(50) ,' .
         'country varchar(50) ,' .
-        'id_sporta int '. 
-        'ADD CONSTRAINT fk_sport FOREIGN KEY (id_sporta) REFERENCES sport(id_sport))'
+        'id_sporta int)'
 	);
 
 	$st->execute();
