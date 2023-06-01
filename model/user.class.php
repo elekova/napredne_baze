bf02fbdaaf4776557cc6d5bb93d9486186bc827d
ikrizanec <85575300+ikrizanec@users.noreply.github.com>
2023-06-01 23:25:51 +0200

    Update user.class.php

--- model/user.class.php
+++ model/user.class.php
@@ -4,6 +4,7 @@ class User
 {
     protected $id_person, $username, $password, $name, $surname;
     protected $date, $city, $region, $registration_sequence, $has_registered, $email;
+    protected $friends, $favorites;
     
     function __construct($id_person, $username, $password, $name, $surname, $date, 
                     $city, $region, $registration_sequence, $has_registered, $email)
@@ -18,7 +19,8 @@ class User
         $this->registration_sequence = $registration_sequence;
         $this->has_registered = $has_registered;
         $this->email = $email;
-
+        $this->friends = []; 
+        $this->favorites = [];
     }
 
     function __get( $prop ){ return $this->$prop; }
