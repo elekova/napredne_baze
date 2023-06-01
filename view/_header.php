4beabf8f503da5d83d02fb4331c8d1b127291d3a
NDobsa <root@NOTEBOOK-J5G72S.localdomain>
2023-06-02 00:22:09 +0200

    view/header, ponešto u view i ponešto u controlleru

--- view/_header.php
+++ view/_header.php
@@ -1,17 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf8">
-	<title>KnjižnicaApp</title>
-	<link rel="stylesheet" href="<?php echo __SITE_URL;?>/css/style.css">
+    <meta charset="UTF-8">
+    <title>My Crew</title>
+    
 </head>
 <body>
-	<h1><?php echo $title; ?></h1>
+    <div style="display: flex; justify-content: space-between; align-items: center; padding: 10px; background-color: #ccc; box-shadow: 0px 0px 10px rgba(0,0,0,0.5);">
+            <h1 style="color: white; font-family: 'Lucida Handwriting', cursive;">My Crew</h1>
 
-	<nav>
-		<ul>
-			<li><a href="<?php echo __SITE_URL; ?>/index.php?rt=users">Popis svih korisnika</a></li>
-			<li><a href="<?php echo __SITE_URL; ?>/index.php?rt=books">Popis svih knjiga</a></li>
-			<li><a href="<?php echo __SITE_URL; ?>/index.php?rt=books/search">Tražilica knjiga po autoru</a></li>
-		</ul>
-	</nav>
+            <div style="text-align: right;">
+                <?php echo $_SESSION['username']; ?>
+                <form action="logout.php" method="post">
+                    <input type="submit" value="Log out">
+                </form>
+            </div>
+     </div>
+    <div style="display: flex; justify-content: space-around; padding: 10px; font-size=20px;">
+        <img src="slikica.png" alt="slikica" style="height: 150px; width: 150px;">
+        <a href="index.php?rt=users/showfriendsquacks" <?php echo $_GET['rt'] == 'users/showfriendsquacks' ? 'class="active"' : ''; ?>>Following</a>
+        <a href="index.php?rt=users/showfollowers" <?php echo $_GET['rt'] == 'users/showfollowers' ? 'class="active"' : ''; ?>>Followers</a>
+   
+    </div>
