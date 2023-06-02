04690ac866245cd999d2e2b29a3fe474eb55c0fa
ikrizanec <ivanakrizanec@hotmail.com>
2023-06-02 20:25:10 +0200

    update

--- controller/personController.php
+++ controller/personController.php
@@ -2,10 +2,46 @@
 
 class PersonController extends BaseController
 {
-    public function index()
+    function index()
     {
         
     }
+
+    function getUser( $username )
+    {
+
+    }
+
+    function addFriendAction($userId, $friendId)
+    {
+        //dohvacanje usera
+        $user = getUser( $userId );
+        $friend = getUser( $friendId );
+
+        //stavaranje veza
+        $user->addFriend($friend);
+        $friend->addFriend($user);
+
+        //spremiti u bazu
+
+        //neka obavijest da je storeno prijteljstvo
+    }
+
+    function removeFriendAction($userId, $friendId)
+    {
+        $user = getUser( $userId );
+        $friend = getUser( $friendId );
+
+        //uklanjanje veze
+        $user->removeFriend($friend);
+        $friend->removeFriend($user);
+
+        //spremiti ubazu
+
+        //neka obavijest
+    }
+
+    // Ostale akcije vezane uz upravljanje vezama
 }
 
 ?>
