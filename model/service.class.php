57fea61b687c0e4cb408c6554105781fefacc80a
NDobsa <root@NOTEBOOK-J5G72S.localdomain>
2023-06-02 18:00:18 +0200

    model/service.class.php

---
+++ model/service.class.php
@@ -0,0 +1,347 @@
+<?php
+
+class LibraryService
+{
+	function getPersonById( $id )
+	{
+		try
+		{
+			$db = DB::getConnection();
+			$st = $db->prepare( 'SELECT id_person, username, name, surname, password FROM person WHERE id=:id' );
+			$st->execute( array( 'id_person' => $id ) );
+		}
+		catch( PDOException $e ) { exit( 'PDO error ' . $e->getMessage() ); }
+
+		$row = $st->fetch();
+		if( $row === false )
+			return null;
+		else
+			return new Person( $row['id_person'], $row['username'], $row['name'], $row['surname'], $row['password'] );
+	}
+
+	function getPersonByName( $name )
+	{
+		try
+		{
+			$db = DB::getConnection();
+			$st = $db->prepare('SELECT id_person, username, name, surname, password FROM person WHERE CONCAT(name, " ", surname) LIKE :name');
+			$st->execute( array( 'name' => $name ) );
+		}
+		catch( PDOException $e ) { exit( 'PDO error ' . $e->getMessage() ); }
+
+		$row = $st->fetch();
+		if( $row === false )
+			return null;
+		else
+			return new Person( $row['id_person'], $row['username'], $row['name'], $row['surname'], $row['password'] );
+	}
+
+	//ovo treba napraviti u skladu s neo4j
+	function getAllFollowers( )
+	{
+		try
+		{
+			$db = DB::getConnection();
+			$st = $db->prepare( 'SELECT id, name, surname, password FROM person ORDER BY surname' );
+			$st->execute();
+		}
+		catch( PDOException $e ) { exit( 'PDO error ' . $e->getMessage() ); }
+
+		$arr = array();
+		while( $row = $st->fetch() )
+		{
+			$arr[] = new Person( $row['id'], $row['name'], $row['surname'], $row['password'] );
+		}
+
+		return $arr;
+	}
+
+	//ovo treba napraviti u skladu sa Neo4j
+	function getAllFollowing( )
+	{
+		try
+		{
+			$db = DB::getConnection();
+			$st = $db->prepare( 'SELECT id, name, surname, password FROM person ORDER BY surname' );
+			$st->execute();
+		}
+		catch( PDOException $e ) { exit( 'PDO error ' . $e->getMessage() ); }
+
+		$arr = array();
+		while( $row = $st->fetch() )
+		{
+			$arr[] = new Person( $row['id'], $row['name'], $row['surname'], $row['password'] );
+		}
+
+		return $arr;
+	}
+
+	// funkcija koja stvara novu vezu u Neo4j
+	function Follow($id_person1, $id_person2) {
+
+	}
+
+	// funkcija koja briše vezu u Neo4j
+	function Unfollow($id_person1, $id_person2) {
+
+	}
+
+	//funkcija koja vraæa sve ponuðene knjige
+	function getAllBooks()
+	{
+		try
+		{
+			$db = DB::getConnection();
+			$st = $db->prepare( 'SELECT id_book, title, author FROM book ORDER BY author' );
+			$st->execute();
+		}
+		catch( PDOException $e ) { exit( 'PDO error ' . $e->getMessage() ); }
+
+		$arr = array();
+		while( $row = $st->fetch() )
+		{
+			$arr[] = new Book( $row['id_book'], $row['title'], $row['author'] );
+		}
+
+		return $arr;
+	}
+
+	//funkcija koja omoguæava korisniku da lajka knjigu $id_book
+	function LikeBook($id_person, $id_book)
+	{
+		try
+		{
+			$db = DB::getConnection();
+			$st = $db->prepare(  );
+			$st->execute();
+		}
+		catch( PDOException $e ) { exit( 'PDO error ' . $e->getMessage() ); }
+
+	}
+
+	// funkcija za svaku osobu vraæa knjige koje se osobi sviðaju
+	function getLikedBooks($id_person)
+	{
+		try
+		{
+			$db = DB::getConnection();
+			$st = $db->prepare( 'SELECT id_book, title, author 
+									FROM book b 
+									JOIN like_book lb ON b.id_book = lb.id_book
+									WHERE lb.id_person = :id_person;' );
+			$st->execute();
+		}
+		catch( PDOException $e ) { exit( 'PDO error ' . $e->getMessage() ); }
+
+		$arr = array();
+		while( $row = $st->fetch() )
+		{
+			$arr[] = new Book( $row['id_book'], $row['title'], $row['author'] );
+		}
+
+		return $arr;
+	}
+
+	//funkcija koja vraæa sve ponuðene sportove
+	function getAllSports()
+	{
+		try
+		{
+			$db = DB::getConnection();
+			$st = $db->prepare( 'SELECT id_sport, type FROM sport ORDER BY type' );
+			$st->execute();
+		}
+		catch( PDOException $e ) { exit( 'PDO error ' . $e->getMessage() ); }
+
+		$arr = array();
+		while( $row = $st->fetch() )
+		{
+			$arr[] = new Sport( $row['id_sport'], $row['type'] );
+		}
+
+		return $arr;
+	}
+	
+	//funkcija koja omoguæava korisniku $id_person da lajka sport $id_sport
+	function LikeSport($id_person, $id_sport)
+	{
+		try
+		{
+			$db = DB::getConnection();
+			$st = $db->prepare(  );
+			$st->execute();
+		}
+		catch( PDOException $e ) { exit( 'PDO error ' . $e->getMessage() ); }
+	}
+
+	//funkcija koja vraæa sportove koji se sviðaju odreðenoj osobi s $id_person
+	function getLikedSports($id_person)
+	{
+		try
+		{
+			$db = DB::getConnection();
+			$st = $db->prepare( 'SELECT id_sport, type 
+									FROM sport s 
+									JOIN like_sport ls ON s.id_sport = ls.id_sport
+									WHERE lb.id_person = :id_person;' );
+			$st->execute();
+		}
+		catch( PDOException $e ) { exit( 'PDO error ' . $e->getMessage() ); }
+
+		$arr = array();
+		while( $row = $st->fetch() )
+		{
+			$arr[] = new Sport( $row['id_sport'], $row['type'] );
+		}
+
+		return $arr;
+	}
+
+	//funkcija koja sve ponuðene bendove
+	function getAllBands()
+	{
+		try
+		{
+			$db = DB::getConnection();
+			$st = $db->prepare( 'SELECT id_band, name, genre FROM band ORDER BY name' );
+			$st->execute();
+		}
+		catch( PDOException $e ) { exit( 'PDO error ' . $e->getMessage() ); }
+
+		$arr = array();
+		while( $row = $st->fetch() )
+		{
+			$arr[] = new Band( $row['id_band'], $row['name'], $row['genre'] );
+		}
+
+		return $arr;
+	}
+
+	//funkcija koja omoguæava korisniku $id_person da lajka bend $id_band
+	function LikeBand($id_person, $id_band)
+	{
+		try
+		{
+			$db = DB::getConnection();
+			$st = $db->prepare(  );
+			$st->execute();
+		}
+		catch( PDOException $e ) { exit( 'PDO error ' . $e->getMessage() ); }
+
+	}
+
+	//funkcija koja vraæa bendove koji se sviðaju odreðenoj osobi s $id_person
+	function getLikedBands($id_person)
+	{
+		try
+		{
+			$db = DB::getConnection();
+			$st = $db->prepare( 'SELECT id_band, name, genre
+									FROM band b 
+									JOIN like_band lb ON b.id_band = lb.id_band
+									WHERE lb.id_person = :id_person;' );
+			$st->execute();
+		}
+		catch( PDOException $e ) { exit( 'PDO error ' . $e->getMessage() ); }
+
+		$arr = array();
+		while( $row = $st->fetch() )
+		{
+			$arr[] = new Band( $row['id_band'], $row['name'], $row['genre'] );
+		}
+
+		return $arr;
+	}
+
+	// funkcija koja vraæa sve ponuðene filmove
+	function getAllMovies()
+	{
+		try
+		{
+			$db = DB::getConnection();
+			$st = $db->prepare( 'SELECT id_movie, title, genre FROM movie ORDER BY title' );
+			$st->execute();
+		}
+		catch( PDOException $e ) { exit( 'PDO error ' . $e->getMessage() ); }
+
+		$arr = array();
+		while( $row = $st->fetch() )
+		{
+			$arr[] = new Movie( $row['id_movie'], $row['title'], $row['genre'] );
+		}
+
+		return $arr;
+	}
+
+	//funkcija koja omoguæava korisniku $id_person da lajka film $id_movie
+	function LikeMovie($id_person, $id_movie)
+	{
+		try
+		{
+			$db = DB::getConnection();
+			$st = $db->prepare(  );
+			$st->execute();
+		}
+		catch( PDOException $e ) { exit( 'PDO error ' . $e->getMessage() ); }
+
+	}
+
+	//funkcija koja vraæa filmove koji se sviðaju odreðenoj osobi s $id_person
+	function getLikedMovies($id_person)
+	{
+		try
+		{
+			$db = DB::getConnection();
+			$st = $db->prepare( 'SELECT id_movie, title, genre
+									FROM movie m
+									JOIN like_movie lm ON m.id_band = lm.id_band
+									WHERE lm.id_person = :id_person;' );
+			$st->execute();
+		}
+		catch( PDOException $e ) { exit( 'PDO error ' . $e->getMessage() ); }
+
+		$arr = array();
+		while( $row = $st->fetch() )
+		{
+			$arr[] = new Movie( $row['id_movie'], $row['title'], $row['genre'] );
+		}
+
+		return $arr;
+	}
+
+	//login, ne znam dal radi (pise Nina)
+	function checkUser($username, $password){
+		$db = DB::getConnection();
+
+		try{
+			$st = $db->prepare( 'SELECT id, password FROM users WHERE name=:username' );
+			$st->execute( array( 'username' => $username ) );
+		}
+		catch( PDOException $e ){
+			echo 'Greska u Service.class.php!';
+			return 0;
+		}
+
+		$row = $st -> fetch();
+		if($row === false){
+			// Taj user ne postoji, upit u bazu nije vratio ništa.
+			return 0;
+		}
+		else{
+			// Postoji user. Dohvati hash njegovog passworda.
+			$hash = $row[ 'password'];
+			// Da li je password dobar?
+			if( password_verify( $_POST['password'], $hash)){
+				return $row['id'];
+			}
+			else{
+				// Nije dobar. Crtaj opet login formu s pripadnom porukom.
+				return 0;
+			}
+		}
+	}
+
+};
+
+?>
+
--- view/_header.php
+++ view/_header.php
@@ -6,19 +6,24 @@
     
 </head>
 <body>
-    <div style="display: flex; justify-content: space-between; align-items: center; padding: 10px; background-color: #ccc; box-shadow: 0px 0px 10px rgba(0,0,0,0.5);">
+    <div style="display: flex; justify-content: space-between; align-items: center; padding: 10px; background-color:rgb(100, 209, 209); box-shadow: 0px 0px 10px rgba(0,0,0,0.5);">
             <h1 style="color: white; font-family: 'Lucida Handwriting', cursive;">My Crew</h1>
 
-            <div style="text-align: right;">
+
+             <div style="text-align: right;">
                 <?php echo $_SESSION['username']; ?>
                 <form action="logout.php" method="post">
                     <input type="submit" value="Log out">
                 </form>
-            </div>
+             </div> 
      </div>
+    <img src="earth.png" alt="slikica" style="height: 150px; width: 150px; float: left;">
     <div style="display: flex; justify-content: space-around; padding: 10px; font-size=20px;">
-        <img src="slikica.png" alt="slikica" style="height: 150px; width: 150px;">
-        <a href="index.php?rt=users/showfriendsquacks" <?php echo $_GET['rt'] == 'users/showfriendsquacks' ? 'class="active"' : ''; ?>>Following</a>
-        <a href="index.php?rt=users/showfollowers" <?php echo $_GET['rt'] == 'users/showfollowers' ? 'class="active"' : ''; ?>>Followers</a>
+        <nav>
+		<ul>
+			<li><a href="index.php?rt=following">Following</a></li>
+			<li><a href="index.php?rt=followers">Followers</a></li>
+        </ul>
+	</nav>
    
     </div>
