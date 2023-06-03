6339b6e7cf147b1020a856cbc84e1e59b95c19dc
ikrizanec <ivanakrizanec@hotmail.com>
2023-06-04 01:30:40 +0200

    database-neo4j

--- model/database.class.php
+++ model/database.class.php
@@ -19,35 +19,110 @@ class DatabaseManager
         $results = NULL;
     }
 
-    function getFriends( $username )
-    {
-        $results = $client->run('MATCH (p:Person) RETURN p');
-    }
-
-    function getFollowing()
-    {
-
-    }
-
-    function getFollowers()
-    {
-
-
-        foreach($results as $result){
-            $node = $result->get('p');
     
-            echo $node->getProperty('name').'<br>';
+    function getUserById( $id )
+    {
+        //ID MORA BITI STRING!
+        $idString = strval( $id );
+        $results = $client->run('MATCH (p:Person {id_person: $id}) RETURN p', ['id' => $idString]);
+
+        foreach($results as $result)
+        {
+            $node = $result->get('p');
+            $param = ['id_person' => $node->getProperty('id_person'), 'name' => $node->getProperty('name'), 'surname' => $node->getProperty('surname')];
+        }
+
+        return $param;
+    } 
+
+    function getUserByUsername( $username )
+    {
+        
+        $results = $client->run('MATCH (p:Person {username: $ime}) RETURN p', ['ime' => $username]);
+        $param = [];
+
+        foreach($results as $result)
+        {
+            $node = $result->get('p');
+            $param = ['name' => $node->getProperty('name'), 'surname' => $node->getProperty('surname')];
+            echo '<br>';
+        }
+        return $param;
+    }
+
+    //vraca listu id-jeva koga sve user s danim id-jem followa
+    //vraca null ako user ne followa nikoga
+    function getFollowing( $id )
+    { 
+        $param = [];
+        $count = 0;
+        $hasResults = FALSE;
+
+        $query = 'MATCH (p:Person {id_person: $id_follows})-[:FOLLOWS]->(followed:Person) RETURN followed.id_person AS followedId';
+        $results = $client->run($query, ['id_follows' => $id]);
+
+        foreach ($results as $result) {
+            $node = $result->get('followedId');
+            $param[] = $node;
+            $hasResults = true;
+            ++$count;
+        }
+        if (!$hasResults) {
+            return NULL;
+        } else {
+            return $param;
         }
     }
 
-    function addFriend()
+    //vraca ko prati usera s danim id-jem
+    //ako nema pratitelja vraca NULL
+    function getFollowers( $id )
     {
+        $param = [];
+        $count = 0;
+        $hasResults = FALSE;
+        
+        $query = 'MATCH (follower:Person)-[:FOLLOWS]->(p:Person {id_person: $id_followed}) RETURN follower.id_person AS followerId';
+        $results = $client->run($query, ['id_followed' => $id]);
+        
+        foreach ($results as $result) {
+            $node = $result->get('followerId');
+            $param[] = $node;
+            $hasResults = true;
+            ++$count;
+        }
+        
+        if (!$hasResults) {
+            return NULL;
+        } else {
+            return $parm;
+        }
+    }
+
+    function addFollow( $follows, $followed)
+    {
+        $query = 'MATCH (p1:Person {username: $follows}), (p2:Person {username: $followed}) CREATE (p1)-[:FOLLOWS]->(p2)';
+        $client->run($query, ['follows' => $follows, 'followed' => $followed]);
 
     }
 
-    function followUser()
+    function removeFollow()
+    {
+        $query = 'MATCH (p1:Person {username: $follows})-[r:FOLLOWS]->(p2:Person {username: $followed}) DELETE r';
+        $client->run($query, ['follows' => $follows, 'followed' => $followed]);
+    }
+
+    function addUser( $username, $name, $surname)
     {
 
+        $query = 'CREATE (p:Person {username: $username, name: $name, surname: $surname}) RETURN p';
+        $result = $client->run($query, ['username' => $username, 'name' => $name, 'surname' => $surname]);
+
+        if ($result->count() > 0) {
+            echo "Novi korisnik je uspješno dodan.";
+        } else {
+            echo "Dodavanje novog korisnika nije uspjelo.";
+        }
     }
 
 }
