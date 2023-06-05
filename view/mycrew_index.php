fb23c5abf271ac410f57be62f979c9ef24c71ed9
elekova <125138212+elekova@users.noreply.github.com>
2023-06-05 12:11:52 +0200

    Update mycrew_index.php

--- view/mycrew_index.php
+++ view/mycrew_index.php
@@ -1,4 +1,9 @@
 <?php require_once __DIR__ . '/_header.php'; ?>
+<?php require_once __DIR__ . '/../app/database/db.class.php'; ?>
 
+
+<?php
+
+ echo "ovo je pocetna stranica" ?>
 
 <?php require_once __DIR__ . '/_footer.php'; ?>
